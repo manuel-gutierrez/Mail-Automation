f7c261a98b465d242855d5e794e7d77ee3e9864e
Manuel <menrique.gutierrez@gmail.com>
2018-04-14 16:16:58 -0500

    Set Up Error Loging

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 const functions = require('firebase-functions');
+const {ErrorReporting} = require('@google-cloud/error-reporting');
+const errors = new ErrorReporting({
+    projectId: 'el-dorado-mailing',
+    keyFilename: '../error_reporting_credentials.json'
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +11,6 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+errors.report('Something broke!');
