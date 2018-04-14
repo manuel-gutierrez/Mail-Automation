56886fa58d64c0d5f58064c5d1e0147ea9c2e07a
Manuel <menrique.gutierrez@gmail.com>
2018-04-14 17:09:54 -0500

    setup typeform

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,16 @@ const functions = require('firebase-functions');
 const {ErrorReporting} = require('@google-cloud/error-reporting');
 const errors = new ErrorReporting({
     projectId: 'el-dorado-mailing',
-    keyFilename: '../error_reporting_credentials.json'
+    keyFilename: './error_reporting_credentials.json'
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.testFunction = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+ errors.report('Something broke!');
+});
 
 
-errors.report('Something broke!');
+
